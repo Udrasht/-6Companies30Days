c6387a17359857f2fbe585e90f5eaa18ae3c96db
Udrasht_pal <udrasht.pal@students.iiit.ac.in>
2024-01-01 22:43:56 +0530

    second Question

---
+++ Goldman Sachs/Combination_Sum_III.cpp
@@ -0,0 +1,92 @@
+// Question Link:- https://leetcode.com/problems/combination-sum-iii/description/
+
+
+
+// Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+// Only numbers 1 through 9 are used.
+// Each number is used at most once.
+// Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+ 
+
+// Example 1:
+
+// Input: k = 3, n = 7
+// Output: [[1,2,4]]
+// Explanation:
+// 1 + 2 + 4 = 7
+// There are no other valid combinations.
+// Example 2:
+
+// Input: k = 3, n = 9
+// Output: [[1,2,6],[1,3,5],[2,3,4]]
+// Explanation:
+// 1 + 2 + 6 = 9
+// 1 + 3 + 5 = 9
+// 2 + 3 + 4 = 9
+// There are no other valid combinations.
+// Example 3:
+
+// Input: k = 4, n = 1
+// Output: []
+// Explanation: There are no valid combinations.
+// Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, there are no valid combination.
+ 
+
+// Constraints:
+
+// 2 <= k <= 9
+// 1 <= n <= 60
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+class Solution {
+public:
+void helper(int ind, int &k ,int &n, vector<vector<int>> &ans,vector<int> temp,int sum){
+    if(temp.size()==k&& sum==n){
+        ans.push_back(temp);
+    }
+    if(temp.size()>k||sum>n){
+        return;
+    }
+    for(int i=ind;i<=9;i++){
+        sum=sum+i;
+        temp.push_back(i);
+        helper(i+1,k,n,ans,temp,sum);
+        temp.pop_back();
+
+        sum=sum-i;
+    }
+}
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        if(n>45){
+            return ans;
+        }
+        vector<int> temp;
+        int sum=0;
+        helper(1,k,n,ans,temp,sum);
+        return ans;
+        
+    }
+};
+
+int main(){
+    int k=3;
+    int n=7;
+    Solution ss;
+    vector<vector<int>> ans=ss.combinationSum3(k,n);
+    for(auto it: ans){
+        for(auto jt: it){
+            cout<<jt<<" ";
+        }
+        cout<<endl;
+    }
+
+}
