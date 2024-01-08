42af267c38fab2f4156a4f94cf7c7a5fb21141f9
Udrasht_pal <udrasht.pal@students.iiit.ac.in>
2024-01-08 23:16:26 +0530

    day 8

---
+++ Microsoft/count_the_number_of_incremovable_subarrays_I.cpp
@@ -0,0 +1,70 @@
+// Question Link:- https://leetcode.com/problems/count-the-number-of-incremovable-subarrays-i/description/
+
+/*
+A subarray of nums is called incremovable if nums becomes strictly increasing on removing the subarray. For example, the subarray [3, 4] is an incremovable subarray of [5, 3, 4, 6, 7] because removing this subarray changes the array [5, 3, 4, 6, 7] to [5, 6, 7] which is strictly increasing.
+
+Return the total number of incremovable subarrays of nums.
+
+Note that an empty array is considered strictly increasing.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: 10
+Explanation: The 10 incremovable subarrays are: [1], [2], [3], [4], [1,2], [2,3], [3,4], [1,2,3], [2,3,4], and [1,2,3,4], because on removing any one of these subarrays nums becomes strictly increasing. Note that you cannot select an empty subarray.
+Example 2:
+
+Input: nums = [6,5,7,8]
+Output: 7
+Explanation: The 7 incremovable subarrays are: [5], [6], [5,7], [6,5], [5,7,8], [6,5,7] and [6,5,7,8].
+It can be shown that there are only 7 incremovable subarrays in nums.
+Example 3:
+
+Input: nums = [8,7,6,6]
+Output: 3
+Explanation: The 3 incremovable subarrays are: [8,7,6], [7,6,6], and [8,7,6,6]. Note that [8,7] is not an incremovable subarray because after removing [8,7] nums becomes [6,6], which is sorted in ascending order but not strictly increasing.
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+  int incremovableSubarrayCount(vector<int>& nums) {
+    int ans = 0;
+    for(int i = 0; i < nums.size(); ++i){
+        for(int j = i; j < nums.size(); ++j){
+            int last = -1, flag = 1;
+            for(int k = 0; k < nums.size(); ++k){
+                if(k >= i && k <= j) continue;
+                if(last >= nums[k]){ flag = 0; break; }
+                last = nums[k];
+            }
+            if(flag) ans++;
+        }
+    }
+    return ans;
+}
+};
+
+int main()
+{
+    Solution ss;
+
+    vector<int>  nums = {6,5,7,8};
+
+    int ans = ss.incremovableSubarrayCount(nums);
+    cout << ans << endl;
+    return 0;
+}
---
+++ Microsoft/maximum_product_of_the_ength_of_two_palindromic_subsequences.cpp
@@ -0,0 +1,116 @@
+// Question Link:-https://leetcode.com/problems/maximum-product-of-the-length-of-two-palindromic-subsequences/description/
+
+/*
+Given a string s, find two disjoint palindromic subsequences of s such that the product of their lengths is maximized. The two subsequences are disjoint if they do not both pick a character at the same index.
+
+Return the maximum possible product of the lengths of the two palindromic subsequences.
+
+A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters. A string is palindromic if it reads the same forward and backward.
+
+ 
+
+Example 1:
+
+example-1
+Input: s = "leetcodecom"
+Output: 9
+Explanation: An optimal solution is to choose "ete" for the 1st subsequence and "cdc" for the 2nd subsequence.
+The product of their lengths is: 3 * 3 = 9.
+Example 2:
+
+Input: s = "bb"
+Output: 1
+Explanation: An optimal solution is to choose "b" (the first character) for the 1st subsequence and "b" (the second character) for the 2nd subsequence.
+The product of their lengths is: 1 * 1 = 1.
+Example 3:
+
+Input: s = "accbcaxxcxx"
+Output: 25
+Explanation: An optimal solution is to choose "accca" for the 1st subsequence and "xxcxx" for the 2nd subsequence.
+The product of their lengths is: 5 * 5 = 25.
+ 
+
+Constraints:
+
+2 <= s.length <= 12
+s consists of lowercase English letters only.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+unordered_map<string,int> mp;
+int n=0;
+bool ispalin(string str){
+    int j=str.size()-1;
+    int i=0;
+    while(i<j){
+        if(str[i]!=str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+    
+    
+}
+  int func(int idx,string str1,string str2,string &str){
+      if(idx==n){
+          if(ispalin(str1)&&ispalin(str2)){
+              return str1.size()*str2.size();
+
+          }
+          else{
+              return 0;
+          }
+      }
+      if(idx>=n){
+          return 0;
+      }
+      int ans=0;
+      string ss=to_string(idx)+"_"+str1+"_"+str2;
+      if(mp.find(ss)!=mp.end()){
+          return mp[ss];
+
+      }
+      
+      for(int i=idx;i<n;i++){
+          int a=func(i+1,str1+str[i],str2,str);
+           int b=func(i+1,str1,str2+str[i],str);
+            int c=func(i+1,str1,str2,str);
+
+            ans=max(ans,max(b,max(a,c)));
+         
+
+      }
+      mp[ss]=ans;
+      return ans;
+
+
+
+  }
+    int maxProduct(string str) {
+        // string str1="";
+        // string str2="";
+         n=str.size();
+        mp.clear();
+    
+        return func(0,"","",str);
+        
+    }
+};
+
+int main()
+{
+    Solution ss;
+
+     string s = "leetcodecom";
+
+    int ans = ss.maxProduct(s);
+    cout << ans << endl;
+    return 0;
+}
